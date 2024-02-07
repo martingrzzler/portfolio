01b068263b6d7ed4700d7449b507f2db9f8007a9
Martin Gressler <martin.gressler@code.berlin>
2024-02-07 19:20:17 +0100

    add info sections with animations

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import "./style.css";
 import * as THREE from "three";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-// import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-// import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
+import { GLTF, GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 namespace View {
-  let scrollY = 0;
+  export let scrollY = 0;
   let targetScrollY = 0;
   const damping = 0.05;
-  export const defaultPosition = new THREE.Vector3(0, 10, 35);
+  export const defaultPosition = new THREE.Vector3(0, 10, 30);
 
   export const camera = new THREE.PerspectiveCamera(
     75,
@@ -27,22 +25,18 @@ namespace View {
     scrollY += (targetScrollY - scrollY) * damping * delta * 60;
 
     const rotation = (scrollY / 10000) * Math.PI * 2;
-    camera.position.x = Math.sin(rotation) * View.defaultPosition.z;
-    camera.position.z = Math.cos(rotation) * View.defaultPosition.z;
+    camera.position.x = Math.sin(rotation) * defaultPosition.z;
+    camera.position.z = Math.cos(rotation) * defaultPosition.z;
     camera.lookAt(lookAt);
+  }
+
+  export function scrollProgress() {
+    return (scrollY % 10000) / 100;
   }
 }
 
 namespace World {
   export const scene = new THREE.Scene();
-  export let isGliding = false;
-  export let mountainCenter = new THREE.Vector3();
-  export let mixer: THREE.AnimationMixer | undefined = undefined;
-  export let climbAction: THREE.AnimationAction | undefined = undefined;
-  export let glideAction: THREE.AnimationAction | undefined = undefined;
-  export let glider: THREE.Object3D | null = null;
-  export let rig: THREE.Object3D | null = null;
-  export let iceAxe: THREE.Object3D | null = null;
 
   export function addFog() {
     scene.fog = new THREE.Fog(0xcccccc, 0, 60);
@@ -57,150 +51,223 @@ namespace World {
     scene.add(directionalLight);
   }
 
-  export function addModel(onLoaded: () => void) {
-    const loader = new GLTFLoader();
-    loader.load("/models/scene-all.glb", (glb) => {
-      scene.add(glb.scene);
-
-      glb.scene.traverse((child) => {
-        if (child.isObject3D) {
-          switch (child.name) {
-            case "rig":
-              rig = child;
-              break;
-            case "axe":
-              iceAxe = child;
-              break;
-            case "glider":
-              glider = child;
-              break;
-            default:
-              break;
-          }
-        }
+  export async function loadModel() {
+    return new Promise<GLTF>((resolve) => {
+      const loader = new GLTFLoader();
+      loader.load("/models/scene-all.glb", (glb) => {
+        resolve(glb);
       });
+    });
+  }
 
-      glider!.visible = false;
+  export function initModel(glb: GLTF) {
+    const mixer = new THREE.AnimationMixer(glb.scene);
+    const meshes = {
+      rig: new THREE.Object3D(),
+      iceAxe: new THREE.Object3D(),
+      glider: new THREE.Object3D(),
+    };
+    glb.scene.traverse((child) => {
+      if (child.isObject3D) {
+        switch (child.name) {
+          case "rig":
+            meshes.rig = child;
+            break;
+          case "axe":
+            meshes.iceAxe = child;
+            break;
+          case "glider":
+            meshes.glider = child;
+            break;
+          default:
+            break;
+        }
+      }
+    });
 
-      mixer = new THREE.AnimationMixer(glb.scene);
-      climbAction = mixer.clipAction(
+    return {
+      ...meshes,
+      mixer,
+      climbAction: mixer.clipAction(
         glb.animations.find((clip) => clip.name === "ascent and cheer")!
-      );
-      glideAction = mixer.clipAction(
+      ),
+      glideAction: mixer.clipAction(
         glb.animations.find((clip) => clip.name === "paraglide")!
-      );
-      climbAction.play();
-      climbAction.paused = true;
-      climbAction.loop = THREE.LoopOnce;
-      climbAction.clampWhenFinished = true;
-
-      const box = new THREE.Box3().setFromObject(glb.scene);
-      mountainCenter = box.getCenter(new THREE.Vector3());
-      onLoaded();
-    });
-  }
-
-  let clampedUnitDir: THREE.Vector3 | null = null;
-  let initFlight = true;
-  export function updateGliding(delta: number) {
-    if (!World.rig) return;
-    if (initFlight) {
-      World.glideAction!.play();
-      World.climbAction!.stop();
-      World.rig.lookAt(View.camera.position);
-      initFlight = false;
-    }
-
-    const speed = 2;
-
-    const dir = View.camera.position.clone().sub(World.rig.position);
-
-    const distance = dir.length();
-    const unitDir = dir.clone().normalize();
-    if (!clampedUnitDir && distance < 20) {
-      console.log("clamped");
-      clampedUnitDir = unitDir.clone();
-      console.log(clampedUnitDir);
-    }
-    const updateVec = clampedUnitDir
-      ? clampedUnitDir.clone().multiplyScalar(speed * delta)
-      : unitDir.multiplyScalar(speed * delta);
-    updateVec.y = -0.5 * delta;
-
-    if (!clampedUnitDir) {
-      World.rig.lookAt(View.camera.position);
-    }
-
-    World.rig.position.add(updateVec);
-  }
-
-  export function update(delta: number) {
-    if (!mixer) return;
-    mixer.update(delta);
+      ),
+      mountainCenter: new THREE.Box3()
+        .setFromObject(glb.scene)
+        .getCenter(new THREE.Vector3()),
+    };
   }
 }
 
-namespace Render {
-  export let canvas: null | HTMLCanvasElement = null;
-  export let renderer: THREE.WebGLRenderer | null = null;
+namespace UI {
+  export function hideLoading(loading: HTMLDivElement) {
+    loading.classList.add("fade-out");
 
-  export function onWindow() {
-    canvas = document.querySelector("canvas")!;
-    renderer = new THREE.WebGLRenderer({
-      canvas,
-      antialias: true,
-      alpha: true,
-    });
+    loading.onanimationend = () => {
+      loading.style.display = "none";
+    };
+  }
 
-    renderer.setClearColor(0x000000, 0);
-    renderer.setSize(innerWidth, innerHeight);
+  export function getElements() {
+    return {
+      loading: document.querySelector(".loading-wrapper") as HTMLDivElement,
+      canvas: document.querySelector("canvas") as HTMLCanvasElement,
+      meContainer: document.querySelector(".me") as HTMLDivElement,
+      welcome: document.querySelector("#welcome") as HTMLDivElement,
+      projects: document.querySelector("#projects") as HTMLDivElement,
+      experience: document.querySelector("#experience") as HTMLDivElement,
+      contact: document.querySelector("#contact") as HTMLDivElement,
+    };
+  }
+  export function slideInMeContainer(me: HTMLDivElement) {
+    me.style.display = "block";
+    me.classList.add("slide-up");
   }
 }
 
-// const controls = new OrbitControls(camera, canvas);
-// controls.zoomToCursor = false;
-// controls.update();
-
-function init() {
-  Render.onWindow();
+window.addEventListener("load", async () => {
+  const {
+    loading,
+    canvas,
+    meContainer,
+    welcome,
+    projects,
+    experience,
+    contact,
+  } = UI.getElements();
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+    alpha: true,
+  });
+  renderer.setClearColor(0x000000, 0);
+  renderer.setSize(innerWidth, innerHeight);
   View.onWindow();
   View.camera.position.copy(View.defaultPosition);
-
-  // World.addFog();
+  World.addFog();
   World.scene.add(View.camera);
   World.addLight();
-  World.addModel(() => {
-    View.camera.lookAt(World.mountainCenter);
-  });
+  const gltf = await World.loadModel();
+  const {
+    glider,
+    climbAction,
+    mountainCenter,
+    mixer,
+    glideAction,
+    rig,
+    iceAxe,
+  } = World.initModel(gltf);
+  glider.visible = false;
 
-  const climbBtn = document.querySelector("#climb")!;
-  climbBtn.addEventListener("click", () => {
-    if (World.climbAction) {
-      World.climbAction.paused = !World.climbAction.paused;
+  climbAction.play();
+  climbAction.paused = true;
+  climbAction.loop = THREE.LoopOnce;
+  climbAction.clampWhenFinished = true;
+
+  World.scene.add(gltf.scene);
+  View.camera.lookAt(mountainCenter);
+  const clock = new THREE.Clock();
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  UI.hideLoading(loading);
+  setTimeout(() => {
+    UI.slideInMeContainer(meContainer);
+  }, 800);
+
+  let ranMask = 0b0000;
+  let isGliding = false;
+  let clampedUnitDir: THREE.Vector3 | null = null;
+  function animate() {
+    requestAnimationFrame(animate);
+    let delta = clock.getDelta();
+    View.updateByScroll(delta, mountainCenter);
+    mixer.update(delta);
+
+    if (View.scrollProgress() >= 5 && !(ranMask & 0b0001)) {
+      ranMask |= 0b0001;
+      welcome.classList.add("slide-west-leave");
+      welcome.onanimationend = () => {
+        welcome.style.display = "none";
+        welcome.classList.remove("slide-west-leave");
+      };
+
+      projects.style.display = "block";
+      projects.classList.add("slide-west-enter");
+      projects.onanimationend = () => {
+        projects.classList.remove("slide-west-enter");
+      };
+
+      climbAction.paused = false;
+    } else if (View.scrollProgress() >= 30 && !(ranMask & 0b0010)) {
+      ranMask |= 0b0010;
+      projects.classList.add("slide-west-leave");
+      projects.onanimationend = () => {
+        projects.style.display = "none";
+        projects.classList.remove("slide-west-leave");
+      };
+
+      experience.style.display = "block";
+      experience.classList.add("slide-west-enter");
+      experience.onanimationend = () => {
+        experience.classList.remove("slide-west-enter");
+      };
+    } else if (View.scrollProgress() >= 60 && !(ranMask & 0b0100)) {
+      ranMask |= 0b0100;
+      experience.classList.add("slide-west-leave");
+      experience.onanimationend = () => {
+        experience.style.display = "none";
+        experience.classList.remove("slide-west-leave");
+      };
+
+      contact.style.display = "block";
+      contact.classList.add("slide-west-enter");
+      contact.onanimationend = () => {
+        contact.classList.remove("slide-west-enter");
+      };
+    } else if (
+      View.scrollProgress() >= 90 &&
+      !(ranMask & 0b1000) &&
+      !climbAction.isRunning()
+    ) {
+      ranMask |= 0b1000;
+      // startGliding
+      console.log("start gliding");
+      glideAction.play();
+      climbAction.stop();
+      iceAxe.visible = false;
+      glider.visible = true;
+      rig.lookAt(View.camera.position);
+      isGliding = true;
     }
-  });
-  const gliderBtn = document.querySelector("#glide")!;
-  gliderBtn.addEventListener("click", () => {
-    World.glider!.visible = true;
-    World.iceAxe!.visible = false;
-    World.isGliding = true;
-  });
-}
 
-const clock = new THREE.Clock();
-function animate() {
-  requestAnimationFrame(animate);
-  let delta = clock.getDelta();
-  View.updateByScroll(delta, World.mountainCenter);
-  World.update(delta);
-  if (World.isGliding) {
-    World.updateGliding(delta);
+    if (isGliding) {
+      const speed = 2;
+      const dir = View.camera.position.clone().sub(rig.position);
+      const distance = dir.length();
+      const unitDir = dir.clone().normalize();
+      if (!clampedUnitDir && distance < 20) {
+        clampedUnitDir = unitDir.clone();
+      }
+      const updateVec = clampedUnitDir
+        ? clampedUnitDir.clone().multiplyScalar(speed * delta)
+        : unitDir.multiplyScalar(speed * delta);
+      updateVec.y = -0.5 * delta;
+
+      if (!clampedUnitDir) {
+        rig.lookAt(
+          new THREE.Vector3(
+            View.camera.position.x,
+            rig.position.y,
+            View.camera.position.z
+          )
+        );
+      }
+
+      rig.position.add(updateVec);
+    }
+
+    renderer.render(World.scene, View.camera);
   }
-
-  Render.renderer!.render(World.scene, View.camera);
-}
-
-window.addEventListener("load", () => {
-  init();
   animate();
 });
